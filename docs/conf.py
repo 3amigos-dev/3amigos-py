4c1dbf8804cdecfd6caa13c4ba9848342a288b48
Tim Gates <tim.gates@iress.com>
2019-07-23 14:30:31 +1000

    Fix the master_doc

--- docs/conf.py
+++ docs/conf.py
@@ -79,3 +79,6 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# Sphinx 2.0 changes from index to contents
+master_doc = 'index'
