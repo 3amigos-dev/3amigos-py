873aa72b688e38375634039a79b2fd0b4b9d5aae
Tim Gates <tim.gates@iress.com>
2019-08-09 08:05:27 +1000

    Fix readarray to strip line ending

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -6,7 +6,7 @@ BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 TOP="$(dirname "${BASEDIR}")"
 
 cd "${TOP}"
-readarray FILES < <(git status -s docs | grep -E ^.D | sed s/^...//)
+readarray -t FILES < <(git status -s docs | grep -E ^.D | sed s/^...//)
 if [ ${#FILES[@]} -ne 0 ] ; then
     git checkout "${FILES[@]}"
 fi
