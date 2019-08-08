da27133b31e77f4983c7d7a0b46f7ea2058eed07
Tim Gates <tim.gates@iress.com>
2019-08-09 07:59:03 +1000

    Fix shellcheck issue

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -6,9 +6,9 @@ BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 TOP="$(dirname "${BASEDIR}")"
 
 cd "${TOP}"
-FILES="$(git status -s docs | grep -E ^.D | sed s/^...//)"
-if [ ! -z "${FILES}" ] ; then
-    git checkout ${FILES} # shellcheck SC2086
+readarray FILES < <(git status -s docs | grep -E ^.D | sed s/^...//)
+if [ ${#FILES[@]} -ne 0 ] ; then
+    git checkout "${FILES[@]}"
 fi
 git checkout -- \
  docs/index.rst
