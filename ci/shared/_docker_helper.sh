7569682ac2ec1461c99eb070b58bb926d822d76a
Tim Gates <tim.gates@iress.com>
2021-01-05 08:05:33 +1100

    Move from pipenv-all to running each python version in its own image

--- ci/shared/_docker_helper.sh
+++ ci/shared/_docker_helper.sh
@@ -2,7 +2,7 @@
 
 function docker_compose_run() {
     USEROPT="$(id -u):$(id -g)"
-    docker-compose build
+    docker-compose build --build-arg PYVER="${PYVER}"
     docker-compose up -d
     docker-compose run --rm -u "${USEROPT}" "$@"
     docker-compose down
